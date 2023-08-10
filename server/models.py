0930aa527aa3458c4ded06eb1cf3b744f4ddd26c
Kamryn Smith <programersign@gmail.com>
2023-08-10 13:19:14 -0700

    assignment complete

--- server/models.py
+++ server/models.py
@@ -15,6 +15,22 @@ class Author(db.Model):
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+    @validates("name")
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+
+        if not name:
+            raise ValueError("Please enter a name.")
+        elif name in names:
+            raise ValueError("Please enter a unique name.")
+        return name
+
+    @validates("phone_number")
+    def validate_phone_number(self, key, numbers):
+        if len(numbers) != 10:
+            raise ValueError("Phone number must be 10 digits long.")
+        return numbers
+
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
@@ -30,3 +46,28 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+    @validates("title")
+    def validate_post(self, key, title):
+        clickbait_titles = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not title:
+            raise ValueError("Please enter a title")
+        elif (clickbait not in title for clickbait in clickbait_titles):
+            raise ValueError("Please enter Clickbaity title.")
+        return title
+
+    @validates("content", "summary")
+    def validate_content(self, key, post_content):
+        if key == "content":
+            if len(post_content) <= 250:
+                raise ValueError("Post content must be at least 250 characters long.")
+        if key == "summary":
+            if len(post_content) >= 250:
+                raise ValueError("Post summary must be at most 250 characters long.")
+        return post_content
+    
+    @validates("category")
+    def validate_category(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError("Category must be either Fick or Non-Fiction.")
+        return category
